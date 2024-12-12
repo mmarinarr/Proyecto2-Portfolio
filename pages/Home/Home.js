2748c9a4bd985f91192e1699e9dcba1749e8e4c0
mmarinarr <marina10paula5@gmail.com>
2024-12-12 17:45:54 +0100

    final

--- pages/Home/Home.js
+++ pages/Home/Home.js
@@ -16,7 +16,7 @@ export const Home = () => {
     <div id="image">
     <p>Pasa el ratón por encima para conocerme mejor!</p>
     <img src="./public/marina2.jpg">
-  
+    <video src="./marina3.mp4" muted loop autoplay id="myVideo"></video>
     </div>
     </div>
     </section>`;
